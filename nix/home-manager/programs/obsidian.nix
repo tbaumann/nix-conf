e4aaf6441939c120e412590a99cebb936e06f2ce
Tilman Baumann <tilman@baumann.name>
2024-04-14 20:03:58 +0100

    Skip obsidian electron override

--- nix/home-manager/programs/obsidian.nix
+++ nix/home-manager/programs/obsidian.nix
@@ -3,14 +3,7 @@
   pkgs,
   lib,
   ...
-}: let
-  obsidian = pkgs.obsidian.override {
-    electron = pkgs.electron_25.overrideAttrs (_: {
-      preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
-      meta.knownVulnerabilities = []; # NixOS/nixpkgs#273611
-    });
-  };
-in {
+}: {
   home.packages = [pkgs.obsidian];
   programs.nixvim.plugins.obsidian = {
     enable = true;
