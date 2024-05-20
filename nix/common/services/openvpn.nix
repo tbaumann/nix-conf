4bd09a01092f124e033c7013454718171fd1cdd6
Tilman Baumann <tilman@baumann.name>
2024-05-20 15:38:48 +0100

    openvpn

--- nix/common/services/openvpn.nix
+++ nix/common/services/openvpn.nix
@@ -9,6 +9,7 @@
         config ${config.age.secrets.stack_baumann-cbxgate_cbxnet_de_ovpn.path}
         auth-user-pass  ${config.age.secrets.stack_baumann-cbxgate_cbxnet_de-password.path}
         '';
+      autoStart = false;
       updateResolvConf = false;
 #      up = "${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved";
 #      down = "${pkgs.update-systemd-resolved}/libexec/openvpn/update-systemd-resolved";
