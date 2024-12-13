f6c64a4ef6a1b56b87d8297e2cf0da6fb5db327b
Tilman Baumann <tilman@baumann.name>
2024-12-13 13:27:58 +0100

    New tailscale tailscale key, few new nvim lsp

--- home-manager/programs/neovim/nixvim/plugins/lsp.nix
+++ home-manager/programs/neovim/nixvim/plugins/lsp.nix
@@ -1,6 +1,11 @@
 {
   programs.nixvim = {
     plugins = {
+      schemastore = {
+        enable = true;
+        yaml.enable = true;
+        json.enable = true;
+      };
       lsp = {
         enable = true;
 
@@ -13,28 +18,78 @@
           };
 
           lspBuf = {
-            gd = "definition";
-            gD = "references";
-            gt = "type_definition";
-            gi = "implementation";
-            K = "hover";
-            "<F2>" = "rename";
+            "<leader>la" = "code_action";
+            "<leader>ld" = "definition";
+            "<leader>lf" = "format";
+            "<leader>lD" = "references";
+            "<leader>lt" = "type_definition";
+            "<leader>li" = "implementation";
+            "<leader>lh" = "hover";
+            "<leader>lr" = "rename";
           };
         };
 
         servers = {
-          helm_ls.enable = true;
+          # broken? tsserver.enable = true;
           clangd.enable = true;
           dockerls.enable = true;
           eslint.enable = true;
+          helm_ls.enable = true;
+          jsonls.enable = true;
           nil_ls.enable = true;
           nixd.enable = true;
           svelte.enable = true;
           tailwindcss.enable = true;
-          # broken? tsserver.enable = true;
+          terraformls.enable = true;
           yamlls.enable = true;
         };
       };
+      which-key.settings.spec = [
+        {
+          __unkeyed = "<leader>l";
+          group = "  LSP";
+        }
+        {
+          __unkeyed = "<leader>la";
+          desc = "Code Action";
+        }
+        {
+          __unkeyed = "<leader>ld";
+          desc = "Definition";
+        }
+        {
+          __unkeyed = "<leader>lD";
+          desc = "References";
+        }
+        {
+          __unkeyed = "<leader>lf";
+          desc = "Format";
+        }
+        {
+          __unkeyed = "<leader>lp";
+          desc = "Prev";
+        }
+        {
+          __unkeyed = "<leader>ln";
+          desc = "Next";
+        }
+        {
+          __unkeyed = "<leader>lt";
+          desc = "Type Definition";
+        }
+        {
+          __unkeyed = "<leader>li";
+          desc = "Implementation";
+        }
+        {
+          __unkeyed = "<leader>lh";
+          desc = "Hover";
+        }
+        {
+          __unkeyed = "<leader>lr";
+          desc = "Rename";
+        }
+      ];
     };
   };
 }
