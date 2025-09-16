5a151a06f0a1e6b354e21e99872f9b2dd1bf6792
Tilman Baumann <tilman@baumann.name>
2025-09-16 09:44:08 +0200

    dynamic timezone on laptops. Plus TLP

--- common/laptop.nix
+++ common/laptop.nix
@@ -9,6 +9,7 @@
   imports = [
   ];
   hardware.bluetooth.enable = true;
+  hardware.sensor.iio.enable = true;
 
   services = {
     blueman.enable = true;
@@ -20,21 +21,30 @@
       percentageAction = 2;
       criticalPowerAction = "PowerOff";
     };
-  };
-  services.power-profiles-daemon.enable = lib.mkForce false;
-  services.auto-cpufreq = {
-    enable = true;
-    settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
+    tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 60;
+        STOP_CHARGE_THRESH_BAT0 = 90;
       };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
+    };
+    power-profiles-daemon.enable = lib.mkForce false;
+    automatic-timezoned.enable = true;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
       };
     };
   };
+  time.timeZone = lib.mkForce null;
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
