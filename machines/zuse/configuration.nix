dee387a3dd688a9aeaa9b1b41743c52c724dd59a
Tilman Baumann <tilman@baumann.name>
2025-11-25 17:13:32 +0100

    Add machine zuse

--- machines/zuse/configuration.nix
+++ machines/zuse/configuration.nix
@@ -1,40 +1,7 @@
 {
-  inputs,
-  pkgs,
-  lib,
-  ...
-}: {
   imports = [
-    ../../modules/shared.nix
 
-    ../../common/core.nix
-    ../../common/core-desktop.nix
-    ../../common/core-pc.nix
   ];
-  topology.self = {
-    hardware.info = "24 core Threadripper workstation";
-    interfaces.eno1 = {
-      network = "home"; # Use the network we define below
-    };
-  };
-  networking.useNetworkd = lib.mkForce true;
 
-  # BTRFS stuff
-  services.btrfs.autoScrub.enable = true;
-  boot.binfmt.emulatedSystems = ["aarch64-linux"];
-
-  services.hardware.openrgb = {
-    enable = true;
-    motherboard = "amd";
-  };
-  programs.coolercontrol.enable = true;
-  # FIXME broken boot.extraModulePackages = with config.boot.kernelPackages; [liquidtux];
-  boot.extraModprobeConfig = "options kvm_amd nested=1";
-  environment.systemPackages = with pkgs; [
-    liquidctl
-  ];
-  #  hardware.openrazer.enable = true;
-  #  hardware.openrazer.users = ["tilli"];
-  networking.hostName = "zuse";
-  system.stateVersion = "23.05"; # Did you read the comment?
+  # New machine!
 }
