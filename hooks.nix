7d95468bd5e8bae41c20612072707419ab9758b0
Tilman Baumann <tilman@baumann.name>
2026-02-02 14:59:47 +0100

    nix fmt: Add nix nixfmt

--- hooks.nix
+++ hooks.nix
@@ -1,16 +1,21 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   imports = [
     inputs.git-hooks-nix.flakeModule
   ];
   perSystem = {
     pre-commit.settings.hooks.nixfmt-rfc-style.enable = true;
-    treefmt.programs.statix = {
-      enable = true;
-      disabled-lints = [
-        "empty_pattern"
-        "redundant_pattern_bind"
-        "unquoted_splice"
-      ];
+    treefmt.programs = {
+      statix = {
+        enable = true;
+        disabled-lints = [
+          "empty_pattern"
+          "redundant_pattern_bind"
+          "unquoted_splice"
+        ];
+      };
+      deadnix.enable = false; # # Not yet
+      nixfmt.enable = true;
     };
   };
 }
