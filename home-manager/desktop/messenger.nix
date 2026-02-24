f2e1b202df11ea022133c833e081849727e37f38
Tilman Baumann <tilman@baumann.name>
2026-02-24 15:29:03 +0100

    s/rambox/ferdium/

--- home-manager/desktop/messenger.nix
+++ home-manager/desktop/messenger.nix
@@ -6,14 +6,13 @@
 # media - control and enjoy audio/video
 {
   nixpkgs.config.allowUnfree = lib.mkForce true;
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
+  nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "rambox"
     ];
   home.packages = with pkgs; [
-    rambox
     signal-desktop-bin
     fractal
+    ferdium
   ];
 }
