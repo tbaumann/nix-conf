a3bd9d06e11b91e259f93cd9ad228625abb70ce3
Tilman Baumann <tilman@baumann.name>
2026-02-25 09:33:05 +0100

    Ucodenix is broken
    See https://github.com/e-tho/ucodenix/issues/69

--- machines/zuse/hardware-configuration.nix
+++ machines/zuse/hardware-configuration.nix
@@ -22,6 +22,7 @@
   boot.kernelParams = [
     #"badram=0x0000000fdbdaa7a8,0xfffffffffffffff8" "memmap=0x0000000fdbdaa7a8$0x0000000fdbdaa7af"
     "initcall_blacklist=dw_i2c_init_driver"
+    "microcode.amd_sha_check=off"
   ];
   boot.blacklistedKernelModules = ["qcserial"];
   nix.settings.system-features = [
@@ -86,16 +87,20 @@
       #    randomEncryption.enable = true;  maybe not for suspend
     }
   ];
+  boot.resumeDevice = "/dev/nvme0n1p3";
   nix.settings = {
     cores = 24;
   };
   networking.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  #  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.enable = true;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  #services.ucodenix.enable = true;
-  #services.ucodenix.cpuModelId = "00830f10";
+  services.ucodenix = {
+    enable = true;
+    #FIXME Broken build, but sources contain microcode for this model-id
+    #cpuModelId = "00830f10";
+  };
 
   services.auto-cpufreq.enable = false;
 }
