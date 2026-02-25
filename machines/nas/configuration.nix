eba55d9f4f1f16fee2c96cb29514d94dd7b23149
Tilman Baumann <tilman@baumann.name>
2026-02-25 09:34:23 +0100

    declaw

--- machines/nas/configuration.nix
+++ machines/nas/configuration.nix
@@ -8,39 +8,29 @@
   imports = with inputs; [
     nixos-sbc.nixosModules.default
     nixos-sbc.nixosModules.boards.raspberrypi.rpi4
-    #clan-core.nixosModules.installer
-    nix-openclaw.nixosModules.openclaw
     ../../modules/shared.nix
-    ../../common/user-group.nix
+    ../../common/core.nix
+    ../../common/tailscale.nix
   ];
-  clan.core.vars.generators.openclaw-token = {
-    files.token.secret = true;
-    script = ''
-      pwgen -c -n 64 1 > $out/token
-    '';
-    runtimeInputs = [pkgs.pwgen];
-  };
-  clan.core.vars.generators.anthropic-token = {
-    share = true;
-    prompts.token.description = "Anthropic token";
-    prompts.token.type = "line";
-    files.token.secret = true;
-    script = ''
-      cp $prompts/* > $out/
-    '';
-  };
-  nixpkgs.overlays = [inputs.nix-openclaw.overlays.default];
-  services.openclaw = {
-    enable = true;
-    providers.anthropic = {
-      oauthTokenFile = config.clan.core.vars.generators.anthropic-token.files.token.path;
-    };
-    instances.default = {
-      gateway.auth.tokenFile = config.clan.core.vars.generators.openclaw-token.files.token.path;
-    };
-  };
+  system.etc.overlay.enable = true;
+  #boot.initrd.systemd.enable = true;
+  system.nixos-init.enable = lib.mkForce false;
+  systemd.oomd.enable = true;
+  documentation.enable = false;
+  documentation.man.enable = false;
+  environment.systemPackages = with pkgs; [
+    bat
+    jq
+  ];
 
   services.openssh.enable = true;
+  services.k3s = {
+    enable = true;
+    extraFlags = [
+      "--write-kubeconfig-group=wheel"
+      "--write-kubeconfig-mode=640"
+    ];
+  };
   sbc.version = "0.3";
   networking.useNetworkd = lib.mkForce true;
   networking.useDHCP = lib.mkDefault true;
