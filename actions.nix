afaad6678f0280129a440fedd0641edc68d01645
Tilman Baumann <tilman@baumann.name>
2026-05-30 17:39:25 +0200

    Correct tag

--- actions.nix
+++ actions.nix
@@ -23,27 +23,50 @@
 
           jobs = {
             check = {
+              permissions = {
+                id-token = "write";
+                contents = "read";
+              };
               steps = [
                 {
                   name = "Checkout repository";
                   uses = "actions/checkout@v4";
                 }
                 {
+                  name = "Install SSH key";
+                  uses = "shimataro/ssh-key-action@v2";
+                  with_ = {
+                    key = "\${{ secrets.DEPLOY_SSH_KEY }}";
+                    known_hosts = "\${{ secrets.SSH_KNOWN_HOSTS }}";
+                  };
+                }
+                /*
+                {
+                  name = "Setup keys";
+                  run = ''
+                    install -m 600 -D /dev/null ~/.ssh/id_rsa
+                    echo "''${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
+                    echo "''${{ secrets.SSH_KNOWN_HOSTS }}" > ~/.ssh/known_hosts
+                  '';
+                }
+                */
+                {
                   name = "Setup Tailscale";
                   uses = "tailscale/github-action@v4";
                   with_ = {
-                    oauth-client-id = "{{ secrets.TS_OAUTH_CLIENT_ID }}";
-                    oauth-secret = "{{ secrets.TS_OAUTH_SECRET }}";
-                    tags = "ci";
+                    authkey = "\${{ secrets.TAILSCALE_AUTHKEY }}";
+                    tags = "tag:ci";
+                    ping = "nas.tail84117.ts.net";
                   };
                 }
                 {
                   name = "Setup Nix";
-                  uses = "nixbuild/nix-quick-install-action@v18";
+                  uses = "DeterminateSystems/determinate-nix-action@v3";
                 }
                 {
-                  name = "Check Nix flake";
-                  run = "nix flake check";
+                  name = "Deploy Clan";
+                  run = "nix run git+https://git.clan.lol/clan/clan-core -- machines update --tags ci";
+                  #run = "nixos-rebuild switch --flake '.#nas' --impure --sudo --target-host root@nas.tail84117.ts.net";
                 }
               ];
             };
