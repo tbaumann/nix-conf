8516fa24a66ceb2de10e0f8ebff2fbb3e81616d1
Tilman Baumann <tilman@baumann.name>
2026-09-02 19:45:55 +0200

    style

--- common/desktops/waybar.nix
+++ common/desktops/waybar.nix
@@ -8,8 +8,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     waybar # the status bar
-    inputs.waybar_media_display.packages.x86_64-linux.default
-    inputs.waybar_weather_display.packages.x86_64-linux.default
+    inputs.waybar_media_display.packages.${pkgs.stdenv.hostPlatform.system}.default
+    inputs.waybar_weather_display.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
   # FIXME maybe waybar-mpris
 }
