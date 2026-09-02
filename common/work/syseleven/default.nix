ab9bf3a5428492a0e61a385b2ecaa50c562d79ee
Tilman Baumann <tilman@baumann.name>
2026-09-02 19:53:20 +0200

    nixfmt

--- common/work/syseleven/default.nix
+++ common/work/syseleven/default.nix
@@ -2,7 +2,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   networking.wg-quick.interfaces.secunet-factory = {
     privateKeyFile = config.sops.secrets.secunet-seven-private-key.path;
     address = [
@@ -25,28 +26,28 @@
     devenv
   ];
   /*
-  nix.settings = {
-    substituters = ["http://cache.factory.secunet.com/factory-1"];
-    trusted-public-keys = ["factory-1:Ai12PqfDkRmLzju4eE5/ucuDGXw4J31d3aTrz4TZKrk="];
-  };
-  nix.buildMachines = [
-    {
-      hostName = "nixbuilder-arm-01.factory.secunet.com";
-      protocol = "ssh-ng";
-      systems = ["aarch64-linux"];
-      maxJobs = 40;
-      speedFactor = 2;
-      supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-    }
-    {
-      hostName = "nixbuilder-amd-01.factory.secunet.com";
-      protocol = "ssh-ng";
-      systems = ["x86_64-linux"];
-      maxJobs = 40;
-      speedFactor = 2;
-      supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-    }
-  ];
+    nix.settings = {
+      substituters = ["http://cache.factory.secunet.com/factory-1"];
+      trusted-public-keys = ["factory-1:Ai12PqfDkRmLzju4eE5/ucuDGXw4J31d3aTrz4TZKrk="];
+    };
+    nix.buildMachines = [
+      {
+        hostName = "nixbuilder-arm-01.factory.secunet.com";
+        protocol = "ssh-ng";
+        systems = ["aarch64-linux"];
+        maxJobs = 40;
+        speedFactor = 2;
+        supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+      }
+      {
+        hostName = "nixbuilder-amd-01.factory.secunet.com";
+        protocol = "ssh-ng";
+        systems = ["x86_64-linux"];
+        maxJobs = 40;
+        speedFactor = 2;
+        supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+      }
+    ];
   */
   sops.secrets.factory-builder-key = {
     group = "wheel";
@@ -65,7 +66,9 @@
   '';
 
   programs.ssh.knownHosts = {
-    "nixbuilder-arm-01.factory.secunet.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9NXi+pEIjOcsgh6uIcLxyGAP1pnp87E0T8dBj8wahG";
-    "nixbuilder-amd-01.factory.secunet.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZM4vpq5mrih7vS8leIZB1wJok4yVRqVJ30L1euVA45";
+    "nixbuilder-arm-01.factory.secunet.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9NXi+pEIjOcsgh6uIcLxyGAP1pnp87E0T8dBj8wahG";
+    "nixbuilder-amd-01.factory.secunet.com".publicKey =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZM4vpq5mrih7vS8leIZB1wJok4yVRqVJ30L1euVA45";
   };
 }
